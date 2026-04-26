624e1b5eb3860322b9d2ffb594c9514ab538cbe8
Wildan-AhmadF <codedbywil@gmail.com>
2026-04-26 13:13:57 +0700

    feat : buat request user

---
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    // mengizinkan semua user melakukan request
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    // validasi data yang masuk
+    public function rules(): array
+    {
+        return [
+            'name'      => ['required', 'string', 'max:255', 'unique:users,name'],
+            'username'  => ['required', 'string', 'max:255', 'unique:users,username'],
+            'email'     => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password'  => ['required', 'string', 'min:8', 'confirmed'],
+            'role_id'   => ['required', 'exists:roles,id'],
+        ];
+    }
+}
